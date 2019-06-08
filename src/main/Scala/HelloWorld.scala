21973a21a77058c008974d3004794b4312272aaa
MuckG <mauricegordon@outlook.com>
2019-06-08 15:58:44 +0100

    Further updates to program

--- src/main/Scala/HelloWorld.scala
+++ src/main/Scala/HelloWorld.scala
@@ -1,14 +1,21 @@
 class HelloWorld {
-  private val s: String = "Hello World!!"
+  val s: String = "Hello World!!"
 
   def sayHello(): Unit = {
     println(s)
   }
 }
 
+class ByeWorld(giveName: String) extends HelloWorld{
+  override val s: String = "I've had enough. See you later " + giveName + "!!"
+}
+
 object myObj{
   def main (args: Array[String]) {
     val hw = new HelloWorld
     hw.sayHello()
+
+    val gw = new ByeWorld("Fred")
+    gw.sayHello()
   }
 }
